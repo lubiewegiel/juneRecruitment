8de75734f79eb9e41e4594fe0b5f5ae4682c9cdc
Unknown <radoslaw.inczewski@gmail.com>
2018-06-23 13:23:22 +0200

    Setup project to use Selenium WebDriver Adds Selenium WebDriver lib

--- FsecureWebdriverJobSearching/src/com/selenium/test/Main.java
+++ FsecureWebdriverJobSearching/src/com/selenium/test/Main.java
@@ -1,8 +1,14 @@
 package com.selenium.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello world");
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.f-secure.com/");
     }
 }
