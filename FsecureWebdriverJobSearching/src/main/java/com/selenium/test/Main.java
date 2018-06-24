efd13ee8c3470df0d9f8d214368a3f265cba1151
Unknown <radoslaw.inczewski@gmail.com>
2018-06-24 12:26:30 +0200

    Creates method that goes though F-secure home page and handles responsiveness

--- FsecureWebdriverJobSearching/src/main/java/com/selenium/test/Main.java
+++ FsecureWebdriverJobSearching/src/main/java/com/selenium/test/Main.java
@@ -1,10 +1,9 @@
 package com.selenium.test;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class Main {
@@ -13,19 +12,16 @@ public class Main {
 
 
         WebDriver driver = new FirefoxDriver();
-
+//        WebDriver driver = new ChromeDriver();
         String baseUrl = "https://www.f-secure.com/";
+
+//        driver.manage().window().setSize(new Dimension(800,1200));
 
         driver.get(baseUrl);
 
         System.out.println(driver.getTitle());
-
-        acceptCookiesUsage(driver);
-
-        String career = handleTranslationsAtHomePage(driver.getCurrentUrl());
-        //TODO: handle responsiveness
-        WebElement careerSectionTextLink = driver.findElement(By.linkText(career));
-        careerSectionTextLink.click();
+        
+        openCareerPageFromHomePage(driver);
 
         driver.findElement(By.xpath("(//a[contains(text(),'See our open positions')])[2]")).click();
 
@@ -36,12 +32,49 @@ public class Main {
 
         Thread.sleep(200);
 
-        WebElement qualityEngineerJobOfferTitle = driver.findElement(By.xpath("//h2[contains(text(), 'Quality Engineer')]"));
+        WebElement qualityEngineerJobOfferTitle = driver
+                .findElement(By.xpath("//h2[contains(text(), 'Quality Engineer')]"));
 
         System.out.println(qualityEngineerJobOfferTitle.getSize().toString());
 
         Assert.assertTrue(qualityEngineerJobOfferTitle.isDisplayed());
         driver.close();
+    }
+
+    public static void openCareerPageFromHomePage(WebDriver driver) throws InterruptedException {
+
+        acceptCookiesUsage(driver);
+
+        String career = handleTranslationsAtHomePage(driver.getCurrentUrl());
+        WebElement careerSectionTextLink;
+        boolean overlayAlreadyHidden = false;
+
+        // Handling with responsiveness - if career link is under 'about' section - works with ChromeDriver
+        try {
+            driver.findElement(By.linkText(career));
+        } catch (NoSuchElementException e) {
+            System.out.println("Career was not visible");
+            driver.findElement(By.xpath("//div[@id='wrapper']/footer/div/div/div[2]/div/a")).click();
+            overlayAlreadyHidden = true;
+            ((JavascriptExecutor) driver)
+                    .executeScript("window.scrollTo(0, document.body.scrollHeight)");
+            Thread.sleep(1000);
+        }
+
+        careerSectionTextLink = driver.findElement(By.linkText(career));
+
+        // For FirefoxDriver 'career' link is visible but not interactable - exception is thrown only after interaction attempt
+        try {
+            careerSectionTextLink.click();
+        }catch (ElementNotInteractableException e1) {
+            System.out.println("Could not scroll to element - Firefox Driver Exception");
+            if (!overlayAlreadyHidden) {
+                driver.findElement(By.xpath("//div[@id='wrapper']/footer/div/div/div[2]/div/a")).click();
+            }
+            ((JavascriptExecutor) driver)
+                    .executeScript("window.scrollTo(0, document.body.scrollHeight)");
+            careerSectionTextLink.click();
+        }
     }
 
     public static String handleTranslationsAtHomePage(String currentUrl) {
