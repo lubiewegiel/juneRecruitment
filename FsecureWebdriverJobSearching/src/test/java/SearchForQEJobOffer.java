d87cea786cdae0dd812a14911ec767d7d08c2eab
Unknown <radoslaw.inczewski@gmail.com>
2018-07-03 21:28:32 +0200

    Extracts string constants Adds scrolling method

--- FsecureWebdriverJobSearching/src/test/java/SearchForQEJobOffer.java
+++ FsecureWebdriverJobSearching/src/test/java/SearchForQEJobOffer.java
@@ -18,6 +18,29 @@ public class SearchForQEJobOffer {
     private WebDriver driver;
     private WebDriverWait wait;
 
+    private final static String BASE_URL = "https://www.f-secure.com/";
+    private final static String POLISH_HOME_PAGE_URL = "https://www.f-secure.com/pl_PL/f-secure";
+    private final static String EXPECTED_HOME_PAGE_TITLE = "F-Secure | Cyber Security Solutions for your Home and Business";
+    private final static String EXPECTED_CAREER_PAGE_URL = "https://www.f-secure.com/en/web/about_global/careers";
+    private final static String EXPECTED_SEE_OUR_OPEN_POSITIONS_BUTTONS_HREF = "https://www.f-secure.com/en/web/about_global/careers/job-openings";
+    private final static String POLISH_CAREERS_TEXT_LINK = "Kariera";
+    private final static String ENGLISH_CAREERS_TEXT_LINK = "Careers";
+
+    private final static String COOKIES_ACCEPTANCE_BUTTON_CSS_SELECTOR = "a.btn.btn-primary";
+    private final static String CITIES_DROPDOWN_LIST_CSS_SELECTOR = "[data-id=\"job-city\"]";
+
+    private final static String COOKIE_PROMPT_BY_PATTERN_ID = "cookie-consent";
+
+    private final static String FELLOW_SHIP_STORIES_BUTTON_XPATH = "//button[contains(text(), 'Fellowship stories')]";
+    private final static String SEE_OUR_OPEN_POSITIONS_BUTTONS_XPATH = "(//a[contains(text(),'See our open positions')])[2]";
+    private final static String QUALITY_ENGINEER_JOB_OFFER_TITLE_XPATH = "//h2[contains(text(), 'Quality Engineer')]";
+    private final static String THIRD_PAGERS_ELEMENTS_XPATH = "(//a[contains(text(),'See our open positions')])";
+    private final static String CITIES_DROPDOWN_LIST_POZNAN_ELEMENT_XPATH = "//*[contains(text(), 'Poznań')]";
+    private final static String FOOTER_MENU_ABOUT_SECTION_XPATH = "//div[@id='wrapper']/footer/div/div/div[2]/div/a";
+
+    private final static String SCROLL_DOWN_TO_THE_BOTTOM_OF_PAGE_JS_SCRIPT ="window.scrollTo(0, document.body.scrollHeight)";
+
+
     @BeforeClass
     public void setUp() {
         driver = new FirefoxDriver();
@@ -26,46 +49,35 @@ public class SearchForQEJobOffer {
     }
 
     @Test
-    public void SearchForQAJobOffer() {
+    public void searchForQAJobOffer() {
+        driver.get(BASE_URL);
 
-        String baseUrl = "https://www.f-secure.com/";
-        String expectedHomePageTitle = "F-Secure | Cyber Security Solutions for your Home and Business";
-        String expectedCareerPageUrl = "https://www.f-secure.com/en/web/about_global/careers";
-        String expectedSeeOurOpenPositionsButtonsHref = "https://www.f-secure.com/en/web/about_global/careers/job-openings";
-
-        String fellowShipStoriesButtonXpath = "//button[contains(text(), 'Fellowship stories')]";
-        String seeOurOpenPositionsButtonsXpath = "(//a[contains(text(),'See our open positions')])[2]";
-        String qualityEngineerJobOfferTitleXpath = "//h2[contains(text(), 'Quality Engineer')]";
-        String thirdPagersElementsXpath = "(//a[contains(text(),'See our open positions')])";
-
-        driver.get(baseUrl);
-
-        Assert.assertEquals(driver.getTitle(), expectedHomePageTitle);
+        Assert.assertEquals(driver.getTitle(), EXPECTED_HOME_PAGE_TITLE);
 
         openCareerPageFromHomePage(driver);
 
-        Assert.assertEquals(driver.getCurrentUrl(),expectedCareerPageUrl);
+        Assert.assertEquals(driver.getCurrentUrl(), EXPECTED_CAREER_PAGE_URL);
 
-        WebElement fellowShipStoriesButton = driver.findElement(By.xpath(fellowShipStoriesButtonXpath));
-        WebElement seeOurOpenPositionsButtons = driver.findElement(By.xpath(seeOurOpenPositionsButtonsXpath));
+        WebElement fellowShipStoriesButton = driver.findElement(By.xpath(FELLOW_SHIP_STORIES_BUTTON_XPATH));
+        WebElement seeOurOpenPositionsButtons = driver.findElement(By.xpath(SEE_OUR_OPEN_POSITIONS_BUTTONS_XPATH));
 
-        Assert.assertTrue(isElementWithByPatternPresented(driver, By.xpath(fellowShipStoriesButtonXpath)));
+        Assert.assertTrue(isElementWithByPatternPresented(driver, By.xpath(FELLOW_SHIP_STORIES_BUTTON_XPATH)));
         Assert.assertTrue(fellowShipStoriesButton.isDisplayed() && fellowShipStoriesButton.isEnabled());
         Assert.assertTrue(seeOurOpenPositionsButtons.isDisplayed() && seeOurOpenPositionsButtons.isEnabled());
         Assert.assertEquals(seeOurOpenPositionsButtons.getAttribute("href"),
-                expectedSeeOurOpenPositionsButtonsHref);
+                EXPECTED_SEE_OUR_OPEN_POSITIONS_BUTTONS_HREF);
 
         seeOurOpenPositionsButtons.click();
 
         filterOffersFromPoznanAtJobOpeningsPage(driver);
         WebElement qualityEngineerJobOfferTitle = driver
-                .findElement(By.xpath(qualityEngineerJobOfferTitleXpath));
+                .findElement(By.xpath(QUALITY_ENGINEER_JOB_OFFER_TITLE_XPATH));
 
         wait.until(ExpectedConditions.visibilityOf(qualityEngineerJobOfferTitle));
 
         Assert.assertTrue(qualityEngineerJobOfferTitle.isDisplayed());
         //assert that there are less than 3 offer pages
-        Assert.assertFalse(isElementWithByPatternPresented(driver, By.xpath((thirdPagersElementsXpath))));
+        Assert.assertFalse(isElementWithByPatternPresented(driver, By.xpath((THIRD_PAGERS_ELEMENTS_XPATH))));
     }
 
     @AfterClass
@@ -74,44 +86,38 @@ public class SearchForQEJobOffer {
     }
 
     private static void filterOffersFromPoznanAtJobOpeningsPage(WebDriver driver) {
-
-        String citiesDropdownListCssSelector = "[data-id=\"job-city\"]";
-        String citiesDropdownListPoznanElementXpath ="//*[contains(text(), 'Poznań')]";
-
-        WebElement citiesDropdownList = driver.findElement(By.cssSelector(citiesDropdownListCssSelector));
+        WebElement citiesDropdownList = driver.findElement(By.cssSelector(CITIES_DROPDOWN_LIST_CSS_SELECTOR));
         citiesDropdownList.click();
 
-        WebElement citiesDropdownListPoznanElement = driver.findElement(By.xpath(citiesDropdownListPoznanElementXpath));
+        WebElement citiesDropdownListPoznanElement = driver.findElement(By.xpath(CITIES_DROPDOWN_LIST_POZNAN_ELEMENT_XPATH));
         citiesDropdownListPoznanElement.click();
     }
 
     private static void openCareerPageFromHomePage(WebDriver driver) {
-
-        WebElement careerSectionTextLink;
+        WebElement careerSectionLinkText;
         Wait<WebDriver> fluentWait = new FluentWait<WebDriver>(driver)
                 .withTimeout(3, TimeUnit.SECONDS)
                 .pollingEvery(1, TimeUnit.SECONDS)
                 .ignoring((NoSuchElementException.class));
-        String career;
-        String footerMenuAboutSectionXpath = "//div[@id='wrapper']/footer/div/div/div[2]/div/a";
+
+        String careerLinkText;
         boolean footerMenuManuallyExpanded = false;
 
         acceptCookiesUsage(driver);
-        career = handleTranslationsAtHomePage(driver.getCurrentUrl());
+        careerLinkText = handleTranslationsAtHomePage(driver.getCurrentUrl());
 
-        final By careerTextLinkByPattern = By.linkText(career);
+        final By careerTextLinkByPattern = By.linkText(careerLinkText);
 
         // Handling with responsiveness - if career link is under 'about' section of footer menu - works with ChromeDriver
         if (!isElementWithByPatternPresented(driver, careerTextLinkByPattern))
         {
             System.out.println("Career was not visible");
-            driver.findElement(By.xpath(footerMenuAboutSectionXpath)).click();
+            driver.findElement(By.xpath(FOOTER_MENU_ABOUT_SECTION_XPATH)).click();
             footerMenuManuallyExpanded = true;
-            ((JavascriptExecutor) driver)
-                    .executeScript("window.scrollTo(0, document.body.scrollHeight)");
+            scrollDownToTheBottomOfPage(driver);
         }
 
-        careerSectionTextLink = fluentWait.until(new Function<WebDriver, WebElement>() {
+        careerSectionLinkText = fluentWait.until(new Function<WebDriver, WebElement>() {
             public WebElement apply(WebDriver driver) {
                 return driver.findElement(careerTextLinkByPattern);
             }
@@ -119,35 +125,39 @@ public class SearchForQEJobOffer {
 
         // For FirefoxDriver 'career' link is visible but not interactable - exception is thrown only after interaction attempt
         try {
-            careerSectionTextLink.click();
+            careerSectionLinkText.click();
         }catch (ElementNotInteractableException e1) {
             System.out.println("Could not scroll to element - Firefox Driver Exception");
             if (!footerMenuManuallyExpanded) {
-                driver.findElement(By.xpath(footerMenuAboutSectionXpath)).click();
+                driver.findElement(By.xpath(FOOTER_MENU_ABOUT_SECTION_XPATH)).click();
             }
-            ((JavascriptExecutor) driver)
-                    .executeScript("window.scrollTo(0, document.body.scrollHeight)");
-            careerSectionTextLink.click();
+            scrollDownToTheBottomOfPage(driver);
+            careerSectionLinkText.click();
         }
+    }
+
+    private static void scrollDownToTheBottomOfPage(WebDriver driver) {
+        ((JavascriptExecutor) driver)
+                .executeScript(SCROLL_DOWN_TO_THE_BOTTOM_OF_PAGE_JS_SCRIPT);
     }
 
     private static String handleTranslationsAtHomePage(String currentUrl) {
         String careersTextLink;
 
-        if (currentUrl.contentEquals("https://www.f-secure.com/pl_PL/f-secure")) {
+        if (currentUrl.contentEquals(POLISH_HOME_PAGE_URL)) {
             System.out.println("Using polish language");
-            careersTextLink = "Kariera";
+            careersTextLink = POLISH_CAREERS_TEXT_LINK;
         } else {
-            careersTextLink = "Careers";
+            careersTextLink = ENGLISH_CAREERS_TEXT_LINK;
         }
         return careersTextLink;
     }
 
     private static void acceptCookiesUsage(WebDriver driver) {
-        By cookiePromptByPattern = By.id("cookie-consent");
+        By cookiePromptByPattern = By.id(COOKIE_PROMPT_BY_PATTERN_ID);
         if (isElementWithByPatternPresented(driver, cookiePromptByPattern)) {
             System.out.println("There is cookie acceptance prompt!");
-            driver.findElement(By.cssSelector("a.btn.btn-primary")).click();
+            driver.findElement(By.cssSelector(COOKIES_ACCEPTANCE_BUTTON_CSS_SELECTOR)).click();
         } else System.out.println("No cookies acceptance needed!");
     }
 
